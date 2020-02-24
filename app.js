2c35404b5f78f45364e9ba0e97b8ac5a7d536a6f
georgelau <laugeorge@gmail.com>
2020-02-24 14:31:04 -0500

    updated UI controller

--- app.js
+++ app.js
@@ -74,7 +74,7 @@ var budgetController = (function() {
 
             // calculate the percentage of income that we spent
             if (data.totals.inc > 0) {
-                data.percentage = (Math.round(data.totals.exp / data.totals.inc)*100);
+                data.percentage = Math.round((data.totals.exp / data.totals.inc)*100);
             }
             else {
                 data.percentage = -1;
@@ -113,7 +113,11 @@ var UIController = (function() {
         inputValue: '.add__value',
         inputBtn: '.add__btn',
         incomeContainer: '.income__list',
-        expensesContainer: '.expenses__list'
+        expensesContainer: '.expenses__list',
+        budgetLabel: '.budget__value',
+        incomeLabel: '.budget__income--value',
+        expensesLabel: '.budget__expenses--value',
+        percentageLabel: '.budget__expenses--percentage'
 
     };
 
@@ -163,6 +167,26 @@ var UIController = (function() {
 
             fieldsArr[0].focus();
         },
+
+        // displaying the budget
+        displayBudget: function(obj) {
+
+            document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+            document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
+            document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
+            
+            if (obj.percentage > 0) {
+
+                document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage + '%';
+            }
+            else {
+                document.querySelector(DOMstrings.percentageLabel).textContent = '---';
+
+            }
+        },
+
+        // get budget function
+        
         
         //pass the DOMstrings to public so we can call in Global App Controller
         getDOMstrings: function() {
@@ -201,6 +225,7 @@ var controller = (function(budgetCtrl, UICtrl) {
 
         // 3. display the budget on the UI
         console.log(budget);
+        UICtrl.displayBudget(budget);
 
     };
 
@@ -231,9 +256,17 @@ var controller = (function(budgetCtrl, UICtrl) {
     return {
         init: function() {
             console.log('applciation starting');
+            UICtrl.displayBudget({
+                budget: 0,
+                totalInc: 0,
+                totalExp: 0,
+                percentage: -1
+
+            });
             setupEventListeners();
         }
     };
+
 
 }) (budgetController, UIController);
 
