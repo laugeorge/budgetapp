b8637b770dd26d3eba9b2c860e46026e1c706cd8
georgelau <laugeorge@gmail.com>
2020-02-26 14:54:13 -0500

    improve UX and added red-focus for expense

--- app.js
+++ app.js
@@ -196,6 +196,15 @@ var UIController = (function() {
         
     };
 
+    var nodeListForEach = function(list,callback) {
+
+        for (var i = 0; i < list.length; i++) {
+
+            callback(list[i], i);
+        }
+    };
+    
+
     return {
         getInput: function() {
 
@@ -274,13 +283,13 @@ var UIController = (function() {
         displayPercentages: function(percentages) {
             var fields = document.querySelectorAll(DOMstrings.expensesPercLabel);
             
-            var nodeListForEach = function(list,callback) {
+            // var nodeListForEach = function(list,callback) {
 
-                for (var i = 0; i < list.length; i++) {
+            //     for (var i = 0; i < list.length; i++) {
 
-                    callback(list[i], i);
-                }
-            };
+            //         callback(list[i], i);
+            //     }
+            // };
 
             nodeListForEach(fields, function(current,index) {
 
@@ -306,6 +315,21 @@ var UIController = (function() {
             year = now.getFullYear();
             document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' ' + year;
 
+        },
+
+        changedType: function() {
+
+            var fields = document.querySelectorAll(
+                DOMstrings.inputType + ',' +
+                DOMstrings.inputDescription + ',' +
+                DOMstrings.inputValue);
+
+            nodeListForEach(fields, function(cur) {
+
+                cur.classList.toggle('red-focus');
+            });
+
+            document.querySelector(DOMstrings.inputBtn).classList.toggle('red');
         },
 
         //pass the DOMstrings to public so we can call in Global App Controller
@@ -335,6 +359,9 @@ var controller = (function(budgetCtrl, UICtrl) {
         
         //using DOM from above
         document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem);
+
+        document.querySelector(DOM.inputType).addEventListener('change', UICtrl.changedType);
+
     };
 
     var updatePercentages = function() {
